d1d83f51843ffedd86865a4a63217788e98405e3
Jozef Legeny <jozef.legeny@mensiatech.com>
2017-03-07 14:23:05 +0100

    [Plugin] Add 2D Topographic Map plugin - the plugin was modified in order to be able to use the mensia cartesian.txt coordinate file - originally it takes x, y, z coordinates whereas mensia coordinates are on -y, x, z axes

--- plugins/visualization/simple-visualization/src/ovp_main.cpp
+++ plugins/visualization/simple-visualization/src/ovp_main.cpp
@@ -1,26 +1,26 @@
 #include "ovp_defines.h"
 
-//#include "algorithms/ovpCAlgorithmSphericalSplineInterpolation.h"
-//#include "box-algorithms/ovpCTopographicMap2DDisplay.h"
+#include "algorithms/ovpCAlgorithmSphericalSplineInterpolation.h"
 
 #include "box-algorithms/ovpCBoxAlgorithmDisplayCueImage.h"
 
+#include "box-algorithms/ovpCBoxAlgorithmTopographicMap2DDisplay.h"
 #include "box-algorithms/ovpCBoxAlgorithmMatrixDisplay.h"
 
 #include "box-algorithms/ovpCBoxAlgorithmKeyboardStimulator.h"
 
 OVP_Declare_Begin()
 
-//	rPluginModuleContext.getTypeManager().registerEnumerationType (OVP_TypeId_SphericalLinearInterpolationType, "Spherical linear interpolation type");
-//	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeId_SphericalLinearInterpolationType, "Spline (potentials)", OVP_TypeId_SphericalLinearInterpolationType_Spline);
-//	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeId_SphericalLinearInterpolationType, "Spline laplacian (currents)",  OVP_TypeId_SphericalLinearInterpolationType_Laplacian);
+	rPluginModuleContext.getTypeManager().registerEnumerationType (OVP_TypeId_SphericalLinearInterpolationType, "Spherical linear interpolation type");
+	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeId_SphericalLinearInterpolationType, "Spline (potentials)", OVP_TypeId_SphericalLinearInterpolationType_Spline);
+	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeId_SphericalLinearInterpolationType, "Spline laplacian (currents)",  OVP_TypeId_SphericalLinearInterpolationType_Laplacian);
 
 
-//	OVP_Declare_New(OpenViBEPlugins::Test::CAlgorithmSphericalSplineInterpolationDesc)
+	OVP_Declare_New(OpenViBEPlugins::Test::CAlgorithmSphericalSplineInterpolationDesc)
 
 	OVP_Declare_New(OpenViBEPlugins::SimpleVisualization::CBoxAlgorithmDisplayCueImageDesc)
 
-//	OVP_Declare_New(OpenViBEPlugins::SimpleVisualization::CTopographicMap2DDisplayDesc)
+	OVP_Declare_New(OpenViBEPlugins::SimpleVisualization::CBoxAlgorithmTopographicMap2DDisplayDesc)
 	OVP_Declare_New(OpenViBEPlugins::SimpleVisualization::CBoxAlgorithmMatrixDisplayDesc)
 
 	OVP_Declare_New(OpenViBEPlugins::SimpleVisualization::CBoxAlgorithmKeyboardStimulatorDesc)
