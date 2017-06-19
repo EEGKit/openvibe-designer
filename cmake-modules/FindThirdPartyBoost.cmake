854e02afe3e561b8528b65dd49247229cee28e59
Thierry Gaugry <thierry.gaugry@inria.fr>
2017-06-19 13:46:00 +0200

    [Build] Use the new CMAKE_CXX_STANDARD + fix path bug
    Tracker Reference: [CORE-721](https://jira.mensiatech.com/browse/CORE-721)

--- cmake-modules/FindThirdPartyBoost.cmake
+++ cmake-modules/FindThirdPartyBoost.cmake
@@ -6,6 +6,8 @@
 
 SET(PATH_BOOST "PATH_BOOST-NOTFOUND")
 FIND_PATH(PATH_BOOST "include/boost/config/auto_link.hpp" PATHS ${LIST_DEPENDENCIES_PATH} PATH_SUFFIXES boost NO_DEFAULT_PATH)
+FIND_PATH(PATH_BOOST "include/boost/config/auto_link.hpp" PATHS ${LIST_DEPENDENCIES_PATH} PATH_SUFFIXES boost)
+
 
 IF(PATH_BOOST)
 	debug_message( "  Found boost includes...")
