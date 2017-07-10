37842bdda730477dc76fef3833600601d141a9ba
Thierry Gaugry <thierry.gaugry@inria.fr>
2017-07-10 12:02:17 +0200

    [Designer] Create a blank scenario only if gui is activated
    No-gui option also hide graphical errors boxes
    
    Tracker Reference: [CORE-968](https://jira.mensiatech.com/browse/CORE-968)

--- applications/platform/designer/src/ovdCApplication.cpp
+++ applications/platform/designer/src/ovdCApplication.cpp
@@ -1597,23 +1597,26 @@ bool CApplication::openScenario(const char* sFileName)
 	{
 		m_rKernelContext.getLogManager() << LogLevel_Warning << "Importing scenario failed...\n";
 
-		std::stringstream l_oStringStream;
-		l_oStringStream << "The requested file: " << sFileName << "\n";
-		l_oStringStream << "may either not be an OpenViBE scenario file, \n";
-		l_oStringStream << "a " + std::string(BRAND_NAME) + " scenario file, \n";
-		l_oStringStream << "be corrupted or not compatible with \n";
-		l_oStringStream << "the selected scenario importer...";
+		if (!(m_eCommandLineFlags&CommandLineFlag_NoGui))
+		{
+			std::stringstream l_oStringStream;
+			l_oStringStream << "The requested file: " << sFileName << "\n";
+			l_oStringStream << "may either not be an OpenViBE scenario file, \n";
+			l_oStringStream << "a " + std::string(BRAND_NAME) + " scenario file, \n";
+			l_oStringStream << "be corrupted or not compatible with \n";
+			l_oStringStream << "the selected scenario importer...";
 
-		::GtkWidget* l_pErrorDialog=gtk_message_dialog_new(
-		            NULL,
-		            GTK_DIALOG_MODAL,
-		            GTK_MESSAGE_WARNING,
-		            GTK_BUTTONS_OK,
-		            "Scenario importation process failed !");
-		gtk_message_dialog_format_secondary_text(
-		            GTK_MESSAGE_DIALOG(l_pErrorDialog), "%s", l_oStringStream.str().c_str());
-		gtk_dialog_run(GTK_DIALOG(l_pErrorDialog));
-		gtk_widget_destroy(l_pErrorDialog);
+			::GtkWidget* l_pErrorDialog=gtk_message_dialog_new(
+						NULL,
+						GTK_DIALOG_MODAL,
+						GTK_MESSAGE_WARNING,
+						GTK_BUTTONS_OK,
+						"Scenario importation process failed !");
+			gtk_message_dialog_format_secondary_text(
+						GTK_MESSAGE_DIALOG(l_pErrorDialog), "%s", l_oStringStream.str().c_str());
+			gtk_dialog_run(GTK_DIALOG(l_pErrorDialog));
+			gtk_widget_destroy(l_pErrorDialog);
+		}
 	}
 	return false;
 }
