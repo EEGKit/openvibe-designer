11665b8b8d61acb71052fd0a11ce6eaa3f967ccf
Jozef Legeny <jozef.legeny@mensiatech.com>
2017-11-23 17:10:02 +0100

    [Scripts] Fix the dependency installer

--- scripts/unix-get-dependencies.sh
+++ scripts/unix-get-dependencies.sh
@@ -3,7 +3,7 @@
 ## dest folder.
 #
 
-DEPENDENCIES="./data.txt"
+DEPENDENCIES="./tests-data.txt"
 CACHE_DIR="../dependencies/arch"
 if [ ! -z $DEPENDENCY_CACHE ]; then
     CACHE_DIR=$DEPENDENCY_CACHE
@@ -77,7 +77,7 @@ function get_dependency() {
 
     if [ ! -f ${CACHE_DIR}/${_dep} ]; then
         echo "Download zip archive: [${DEPENDENCY_SERVER}/${_dep}]"
-        wget --user=$user --password=$pass -nv -P ${CACHE_DIR} ${DEPENDENCY_SERVER}/${_dep}
+        wget --user=$user --password=$pass -nv -P ${CACHE_DIR}/${_dep%/*} ${DEPENDENCY_SERVER}/${_dep}
     fi
 }
 
