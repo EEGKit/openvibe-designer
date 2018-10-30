0f8ff03af6936eee80df85c8451dbf684deee4ef
Jozef Legény <jozef.legeny@mensiatech.com>
2018-10-30 11:21:14 +0100

    [Lib Adv Viz] Added a simple test for the crash

---
+++ libraries/lib-advanced-visualization/test/renderer/main_test_renderer.cpp
@@ -0,0 +1,17 @@
+#include <mensia/advanced-visualization.h>
+
+using namespace Mensia::AdvancedVisualization;
+int main()
+{
+	auto m_pRendererContext = IRendererContext::create();
+	m_pRendererContext->clear();
+
+	m_pRendererContext->setDataType(IRendererContext::DataType_Matrix);
+
+	IRenderer* rend = IRenderer::create(IRenderer::RendererType_Bitmap, false);
+
+	rend->setChannelCount(10);
+	float* tmp = new float[666];
+	rend->feed(tmp);
+	rend->refresh(*m_pRendererContext);
+}
