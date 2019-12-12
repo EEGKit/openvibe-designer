0520ba0c78ca41fd2b953b05d81847010c11856f
Thibaut <thibaut.monseigne@inria.fr>
2019-12-12 16:29:33 +0100

    Step Designer Comment

--- applications/platform/designer/src/ovdCCommentProxy.h
+++ applications/platform/designer/src/ovdCCommentProxy.h
@@ -6,16 +6,16 @@
 
 namespace OpenViBEDesigner
 {
-	class CCommentProxy
+	class CCommentProxy final
 	{
 	public:
 
-		CCommentProxy(const OpenViBE::Kernel::IKernelContext& ctx, const OpenViBE::Kernel::IComment& rComment);
-		CCommentProxy(const OpenViBE::Kernel::IKernelContext& ctx, OpenViBE::Kernel::IScenario& scenario, const OpenViBE::CIdentifier& rCommentIdentifier);
-		virtual ~CCommentProxy();
+		CCommentProxy(const OpenViBE::Kernel::IKernelContext& ctx, const OpenViBE::Kernel::IComment& comment);
+		CCommentProxy(const OpenViBE::Kernel::IKernelContext& ctx, OpenViBE::Kernel::IScenario& scenario, const OpenViBE::CIdentifier& commentID);
+		~CCommentProxy() { if (!m_applied) { this->apply(); } }
 
-		operator OpenViBE::Kernel::IComment*() const { return m_pComment; }
-		operator const OpenViBE::Kernel::IComment*() const { return m_pConstComment; }
+		operator OpenViBE::Kernel::IComment*() const { return m_comment; }
+		operator const OpenViBE::Kernel::IComment*() const { return m_constComment; }
 
 		int getWidth(GtkWidget* widget) const;
 		int getHeight(GtkWidget* widget) const;
@@ -27,19 +27,19 @@ namespace OpenViBEDesigner
 
 		void apply();
 
-		virtual const char* getLabel() const;
+		const char* getLabel() const;
 
 	protected:
 
-		virtual void updateSize(GtkWidget* widget, const char* sText, int* pXSize, int* pYSize) const;
+		static void updateSize(GtkWidget* widget, const char* text, int* xSize, int* ySize);
 
 		const OpenViBE::Kernel::IKernelContext& m_kernelCtx;
-		const OpenViBE::Kernel::IComment* m_pConstComment = nullptr;
-		OpenViBE::Kernel::IComment* m_pComment = nullptr;
+		const OpenViBE::Kernel::IComment* m_constComment = nullptr;
+		OpenViBE::Kernel::IComment* m_comment = nullptr;
 		bool m_applied = false;
 		int m_centerX = 0;
 		int m_centerY = 0;
-		mutable std::string m_sLabel;
+		mutable std::string m_label;
 	};
-};
+} // namespace OpenViBEDesigner
 
